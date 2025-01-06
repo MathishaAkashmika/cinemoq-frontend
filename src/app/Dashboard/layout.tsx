fa315d3969d54907737223eedc6d3045c28f1e18
Hashan Perera <140963591+yasuridevindi@users.noreply.github.com>
2025-01-06 21:07:53 +0530

    Yasuri Nav bar issue

--- src/app/Dashboard/layout.tsx
+++ src/app/Dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import Sidebar from "./components/sidebar";
 import Header from "./components/header";
+import Navbar from "../components/Navbar";
 
 export default function AdminLayout({
   children,
@@ -7,15 +8,18 @@ export default function AdminLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex h-screen bg-gradient-to-b from-purple-800 via-black to-black text-white">
-      {/* Sidebar */}
-      <Sidebar />
+    <>
+      <Navbar />
+      <div className="flex h-screen bg-gradient-to-b from-purple-800 via-black to-black text-white">
+        {/* Sidebar */}
+        <Sidebar />
 
-      {/* Main Content */}
-      <div className="flex-1 flex flex-col">
-        <Header />
-        <main className="p-6 overflow-auto">{children}</main>
+        {/* Main Content */}
+        <div className="flex-1 flex flex-col">
+          <Header />
+          <main className="p-6 overflow-auto">{children}</main>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
